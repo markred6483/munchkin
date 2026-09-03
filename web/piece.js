fe03d4c112585958e2b11decc38e6cb3944d8176
Markred <>
2026-09-03 23:26:52 +0200

    simplify for AI

--- web/piece.js
+++ web/piece.js
@@ -1,6 +1,6 @@
 import { getDomId } from './utils.js';
 
-export class BoardPiece { // TODO? should not export this
+export class BoardPiece {
 
     constructor(args = {}) {
         this._view = document.createElement('div');
@@ -15,7 +15,6 @@ export class BoardPiece { // TODO? should not export this
             else if (!this._dragMode)
                 this.select();
             else {
-                // TODO? el.setPointerCapture(e.pointerId);
                 this._view.classList.add('board-piece--dragging');
                 this._dragState = {
                     offsetX: this.left - e.detail.x,
@@ -31,7 +30,6 @@ export class BoardPiece { // TODO? should not export this
         });
         this._view.addEventListener('board-pointerup', (e) => {
             if (!this._dragState) return;
-            // TODO? try { el.releasePointerCapture(pointerId); } catch (err) {}
             this._dragState = null;
             this._view.classList.remove('board-piece--dragging');
             this._board.notify(this, 'drag');
@@ -59,11 +57,6 @@ export class BoardPiece { // TODO? should not export this
         this._view.style.left = `${x}px`;
         this._view.style.top = `${y}px`;
         setTimeout(() => this._view.classList.remove('board-piece--smooth-move'), 300);
-        /* TODO solve these issues:
-            1) the duration of the animation is defined twice (here and in the CSS)
-            2) if a piece is moved twice in less than 300ms, the 2nd animation gets interrupted and the piece is moved suddenly
-            3) the duration of the animation is hard-coded instead of being dynamic (based on the distance)
-        */
         this._board.notify(this, 'move');
     }
 
@@ -197,135 +190,6 @@ class BoardCard extends BoardPiece {
 
     set faceUp(faceUp) { // true / false
         // TODO animation if it's to be flipped
-    }
-
-}
-
-
-class BoardDice extends BoardPiece {
-
-    constructor(args = {}) {
-        super(args);
-        // TODO
-    }
-
-    get view() {
-        const v = super._view;
-        // TODO
-        return v;
-    }
-
-    get contextMenu() {
-        const cm = super.contextMenu;
-        // TODO + button to "roll" this dice
-        return cm;
-    }
-
-    roll() {
-        // TODO start rolling animation
-        setTimeout(() => this.value = Math.floor(Math.random() * 6) + 1, 3000);
-    }
-
-    set value(value) {
-        // TODO stop rolling animation, if any
-        // TODO change the value of the dice
-    }
-
-}
-
-class BoardNote extends BoardPiece {
-
-    // TODO This is a multiline unicode text
-    // TODO scrollbars appear if the text doesn't fit in the box
-
-    constructor(args = {}) {
-        super(args);
-        /*
-          args = {
-            text: 'any string' | null, // default = ''
-            style: 'N' | 'B', // default = 'N'
-            size: 'S' | 'M' | 'L' | null, // default = 'M'
-            align: 'L' | 'C' | 'R' | null // default = 'C'
-          }
-          # if args.text is null or undefined,
-          ## it means this is being created by the user:
-          ## text can be edited right away (initial write mode)
-          ## without the need of the user to select the action within the context menu
-          # if args.text is not null and is not undefined,
-          ## it means this is being created programmatically:
-          ## to edit it the user have to select the action withing the context menu (initial read mode)
-        */
-    }
-
-    get view() {
-        const v = super._view;
-        // TODO
-        return v;
-    }
-
-    get contextMenu() {
-        const cm = super.contextMenu;
-        /* TODO
-            + button to "resize" this Piece from any angle and any side
-              * highlighted when active (resize-mode)
-              * deactivated when pressed again
-              * deactivated when this Piece is unselected
-            + button to "edit" the text
-              * highlighted when active (edit-mode)
-              * deactivated when pressed again
-              * deactivated when the Piece is unselected
-              * deactivated when "Enter" is pressed
-            + button to toggle normal/bold style
-            + button to toggle small/medium/big size
-        */
-        return cm;
-    }
-
-    set text(text) {
-        // TODO change content
-    }
-
-    set style(style) {
-        // TODO bold/normal
-    }
-
-    set size(size) {
-        // TODO small/medium/big
-    }
-
-    set align(align) {
-        // TODO left/center/right
-    }
-
-}
-
-class BoardArea extends BoardPiece {
-    /*
-    Area is a special piece:
-    - TODO children move together with the Area, but can still be selected and moved individually
-    - TODO other pieces can be attached (becoming children) to it by moving them fully inside the Area
-    - TODO other pieces can be detached (no longer children) from it by moving them even partially outside of the Area
-    - TODO destroying an Area destroys the children too
-    - TODO when selecting the Area, all the children appear selected
-    */
-
-    get contextMenu() {
-        const cm = super.contextMenu;
-        /* TODO
-            + button to "resize" this Piece from any angle and any side
-              * highlighted when active (resize-mode)
-              * deactivated when pressed again
-              * deactivated when this Piece is unselected
-        */
-        return cm;
-    }
-
-    addChild(piece) {
-        // TODO append piece to this BoardArea's view
-    }
-
-    removeChild(piece) {
-        // TODO
     }
 
 }
